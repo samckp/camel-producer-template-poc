5d0bb977316af1a66ac1b21106dcf315e2839535
samckp <sam.mca.liet@gmail.com>
2020-06-27 07:24:43 +0530

    testcases added

--- src/test/java/com/camel/producer/SimpleRouteTests.java
+++ src/test/java/com/camel/producer/SimpleRouteTests.java
@@ -1,21 +1,21 @@
 package com.camel.producer;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.*;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-class TimerRouteTests extends CamelTestSupport {
+class SimpleRouteTests extends CamelTestSupport {
 
-	@Produce("{{timeRoute}}")
+	@Produce("direct:simpleRoute")
 	private ProducerTemplate producer;
 
-	@EndpointInject("mock:out")
+	@EndpointInject("mock:output")
 	MockEndpoint mock;
 
 	@Test
@@ -25,7 +25,7 @@ class TimerRouteTests extends CamelTestSupport {
 		String input="test message";
 		mock.expectedBodiesReceived(input);
 
-		producer.sendBody("{{timeRoute}}");
+		producer.sendBody("direct:simpleRoute",input );
 		mock.assertIsSatisfied();
 	}
 }
